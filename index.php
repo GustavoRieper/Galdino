e647732e094ab2ae78820c8c3bdef19853c317b6
Gustavo Rieper <gustavorieper@gmail.com>
2018-06-29 17:37:08 -0300

    Desenvolvimento do Menu
    Desenvolvido menu superior do site.

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/index.css">
+        <title>Construtora Galdino</title>
+    </head>
+    
+    <body>
+        <?php include 'menu.php'; ?>
+    </body>
+</html>
---
+++ menu.php
@@ -0,0 +1,18 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="css/menu.css">
+</head>
+
+<nav class="menu">
+    <div class="content_menu">        
+        <img src="img/logo.png" title="Construtora Galdino">        
+        <div id="nome">
+            <h1>Construtora Galdino</h1>
+        </div>
+        <ul>
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Projetos</a></li>
+            <li><a href="#">Contatos</a></li>
+            <li><a href="#">Sobre</a></li>
+        </ul>
+    </div>
+</nav>
