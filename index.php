9186239a18a6c4e1ef6d766860005b164e6396dc
Gustavo Rieper <gustavorieper@gmail.com>
2018-06-30 18:54:23 -0300

    Desenvolvimeno de Banner
    Desenvolvimento do banner e projetos

--- index.php
+++ index.php
@@ -7,5 +7,43 @@
     
     <body>
         <?php include 'menu.php'; ?>
+        <div class="banner">
+<!--            <img src="img/banner.jpg">-->
+            <div id="plano_banner"></div>
+            <div class="itens_banner">
+                <h2>Um toque de qualidade na sua obra
+                    <a href="#"><span id="btn">Quero conhecer</span></a></h2>
+                
+            </div>
+        </div>
+        <div class="post">
+            <div class="content_post">
+                <h2>Projetos</h2>
+                <div id="post1">
+                    <div class="cards">
+                        <div id="fd1_img"></div>
+                        <img id="img_post" src="img/casa_alvenaria.jpg">
+                        <h1>Casa de Alvenaria</h1>
+                        <p>Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio. Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio.</p>
+                    </div>                    
+                </div>
+                <div id="post2">
+                    <div class="cards">
+                        <img id="img_post" src="img/casa_alvenaria.jpg">
+                        <h1>Casa de Alvenaria</h1>
+                        <p>Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio. Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio.</p>
+                    </div>                    
+                </div>
+                <div id="post3">
+                    <div class="cards">
+                        <div id="fd3_img"></div>
+                        <img id="img_post" src="img/casa_alvenaria.jpg">
+                        <h1>Casa de Alvenaria</h1>
+                        <p>Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio. Descrição sobre a construção da casa de alvenaria. Detalhes breves apenas para fazer a propaganda do negócio.</p>
+                    </div>                    
+                </div>
+            </div>
+            <div class=""></div>
+        </div>
     </body>
 </html>
